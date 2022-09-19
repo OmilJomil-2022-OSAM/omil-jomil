e2e18f008874ae3b7eb8a26514492abd1a719d44
greenrain78 <kdwkdw0078@gmail.com>
2022-09-19 11:37:26 +0000

    db 생성중

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -1,4 +1,15 @@
 import os 
 
 DB_NAME = os.environ['DB_NAME']
-print(DB_NAME)
+
+db_list = {
+    'docker' : {
+        'ip': 'db',
+        'port' : 5432,
+        'user' : 'user',
+        'pw': '1234',
+        'name': 'dress',
+    }
+}
+
+DB_INFO = db_list[DB_NAME]
